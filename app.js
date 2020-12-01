9713c7af0cd9c16ee49166858dd64bab9baf49fd
Satish maurya <satishmaurya15216@gmail.com>
2020-12-01 08:43:04 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var button = document.querySelector(".section-btn");
+var textarea = document.querySelector(".textarea");
+var output = document.querySelector(".output");
+
+
+function error(){
+    alert("yup we are sorry!! try after an hour")
+}
+
+function btnclick(){
+fetch(`https://api.funtranslations.com/translate/shakespeare.json?text=${textarea.value}`)
+.then(response=>response.json())
+.then(value=>{
+    output.innerText=value.contents.translated
+})
+.catch(error)
+}
+button.addEventListener("click", btnclick)
